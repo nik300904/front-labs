8939c538cdc09ed59fe3b54cc8f3eb653047a29e
nik300904 <nikitak3009@gmail.com>
2024-01-11 17:10:51 +0300

    lab 3 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,25 @@
-import { useEffect, useState } from 'react';
-import { Table, Button } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
-import "./App.css";
-import axios from 'axios';
+import { useState } from "react";
+import MainRouter from "./app/routing";
+import Header from "./components/Header";
 
-interface DataType {
-  country: string;
-  name: string;
-}
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'Страна',
-    dataIndex: 'country',
-    key: 'country',
-  },
-  {
-    title: 'Название учебного заведения',
-    dataIndex: 'name',
-    key: 'name',
-  },
-]
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const LIMIT_LIST_SCHOOL = 5;
-
-  const getUniversity = async (limit: number, page: number) => {
-    const offset = page * LIMIT_LIST_SCHOOL;
-    const response = await axios.get(`http://universities.hipolabs.com/search?offset=${offset}&limit=${limit}`);
-    return response.data;
-  }
-
-  const [page, setPage] = useState<number>(1);
-  const [data, setData] = useState<DataType[]>([]);
-
-  useEffect(() => {
-    getUniversity(LIMIT_LIST_SCHOOL, page - 1).then((data) => setData(data))
-  }, [page])
+  const handleAuthentication = () => {
+    setIsAuthenticated((prevState) => {
+      return !prevState;
+    });
+  };
 
   return (
     <>
-      <Table columns={columns} dataSource={data} pagination={false}/>
-      <div className="buttons">
-        <Button onClick={(() => setPage(page - 1))} disabled={!(page - 1)}>Назад</Button>
-        <Button>{ page }</Button>
-        <Button onClick={(() => setPage(page + 1))}>Вперед</Button>
-      </div>
+      <Header
+        isAuthenticated={isAuthenticated}
+        handleAuthentication={handleAuthentication}
+      />
+      <MainRouter />
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/app/routing/config.tsx
@@ -0,0 +1,3 @@
+export const PAGINTAION_ROUTE = '/pagination';
+export const ANOTHER_ROUTE = '/another';
+export const NEW_ROUTE = '/new';
